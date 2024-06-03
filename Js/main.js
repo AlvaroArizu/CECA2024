6bd64e480f747959f9ead2c62512466842cdd1dd
AlvaroArizu <alvaro.arizu2000@gmail.com>
2024-06-03 16:20:32 -0300

    NavBar y Comite Cientifico

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Event listener para el botón de la hamburguesa
     document.querySelector('.navbar-toggler').addEventListener('click', function() {
         const navCollapse = document.querySelector('.navbar-collapse');
         navCollapse.classList.toggle('active'); // Toggle class "active" on navbar-collapse
     });
+
+    // Obtener todos los elementos del menú
+    const menuItems = document.querySelectorAll('.navbar-nav .nav-item a');
+
+    // Agregar evento de clic a cada elemento del menú
+    menuItems.forEach(function(menuItem) {
+        menuItem.addEventListener('click', function() {
+            const navCollapse = document.querySelector('.navbar-collapse');
+            navCollapse.classList.remove('active'); // Retraer el menú
+        });
+    });
 });
+
 
